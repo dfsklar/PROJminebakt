2ab3466fd2b549b5519b914b64503a8c9480bf66
root <root@li479-42.members.linode.com>
2014-04-26 12:44:15 -0400

    aOperationsl:wq

--- query_isbn_list.js
+++ query_isbn_list.js
@@ -1,13 +1,29 @@
 var page = require('webpage').create();
-var fs = require('fs');
 
-var isbns = [
-  '9780451413888',
-  '9780674430006'
-];
+var fs = require('fs'),
+    system = require('system');
 
-console.log(isbns.length);
+var content = '',
+    f = null,
+    lines = null,
+    eol = system.os.name == 'windows' ? "\r\n" : "\n";
 
+try {
+    f = fs.open("/tmp/isbnlist.txt", "r");
+    content = f.read();
+} catch (e) {
+    console.log(e);
+}
+
+if (f) {
+    f.close();
+}
+
+var isbns = [];
+
+if (content) {
+    isbns = content.split(eol);
+}
 
 
 
@@ -38,21 +54,21 @@ function waitFor(testFx, onReady, timeOutMillis) {
   start = new Date().getTime(),
   condition = false,
   interval = setInterval(function() {
-    console.log("'waitFor()' entered");
+//    console.log("'waitFor()' entered");
     if ( ( (new Date().getTime() - start) < maxtimeOutMillis) && !condition ) {
       // If not time-out yet and condition not yet fulfilled
       condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-	    console.log(condition);
+//	    console.log(condition);
     } else {
       if(!condition) {
         // If condition still not fulfilled (timeout but condition is 'false')
-        console.log("'waitFor()' timeout -- going ahead and running onReady anyway");
+//        console.log("'waitFor()' timeout -- going ahead and running onReady anyway");
         typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
         clearInterval(interval); //< Stop this interval
         // phantom.exit(1);
       } else {
         // Condition fulfilled (timeout and/or condition is 'true')
-        console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+//        console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
         typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
         clearInterval(interval); //< Stop this interval
       }
@@ -67,9 +83,9 @@ function performOneQuery() {
   if (isbns.length == 0)
     phantom.exit();
 
-  var isbn = isbns.pop();
+  var isbn = isbns.shift();
 
-  console.log("******************** " + isbn + " ******************************");
+  console.log("* " + isbn);
 
   var btn = page.evaluate(function(isbn) {
     $('#ctl00_PlaceHolderMasterPageHeader_ucSearchBox_SearchPhraseTextBox').val(isbn);
@@ -80,25 +96,28 @@ function performOneQuery() {
     $('#ctl00_QSHeaderPlaceHolder_ucSearchBox_SearchPhraseTextBox_wrapper').val(isbn);
     $('#ctl00_QSHeaderPlaceHolder_ucSearchBox_SearchPhraseTextBox_text').val(isbn);
 
-	  console.log("Submitting the form via a simulated ENTER key...");
+//	  console.log("Submitting the form via a simulated ENTER key...");
     SearchBox.Search();
   }, isbn);
 
-  console.log("Waiting for response to click event");
+//  console.log("Waiting for response to click event");
   waitFor("false", 
           function(){
-            console.log("Emitting PNG and captured DOM for ISBN" + isbn);
+            console.log("Emitting for ISBN" + isbn);
             var minedContent = 
               page.evaluate(function() {
-                var strEmit = "SKLARSKI-TITLE:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_lblTitle').html() + "\n";
-                strEmit += ("SKLARSKI-AUTHOR:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_authors').html() + "\n");
-                strEmit += ("SKLARSKI-INFO:" + $('#divProductionInformation').html() + "\n");
-                strEmit += ("SKLARSKI-DETAILS:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_bookInfoPanel').html() + "\n");
+                var strEmit = "ZTITLE:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_lblTitle').html() + "\n";
+                strEmit += ("ZAUTHOR:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_authors').html() + "\n");
+                strEmit += ("ZPRODINF:" + $('#divProductionInformation').html() + "\n");
+                strEmit += ("ZPHYS:" + $('#divPhysical').html() + "\n");
+                strEmit += ("ZLOINF:" + $('#divDetailLocation').html() + "\n");
+		strEmit += ("ZBTINF:" + $('#divBTSpecificData').html() + "\n");
+                strEmit += ("ZDETAILS:" + $('#ctl00_BrowseBodyInner_ProductDetailsUserControl_bookInfoPanel').html() + "\n");
                 return strEmit;
               });
-            page.render("exports/"+isbn+".png");
+//            page.render("exports/"+isbn+".png");
             fs.write("exports/"+isbn+".data", minedContent);
-            console.log("ABOUT TO CALL pOQ-1 (deferred)");
+//            console.log("ABOUT TO CALL pOQ-1 (deferred)");
             setTimeout(function(){performOneQuery();}, 1000);
           },
           15000);
@@ -109,7 +128,7 @@ var poq2triggered = false;
 page.open('https://ts360.baker-taylor.com/Pages/default.aspx', function(){
   if (!poq2triggered) {
     poq2triggered = true;
-    console.log("ABOUT TO CALL pOQ-2");
+//    console.log("ABOUT TO CALL pOQ-2");
     performOneQuery();
   }
 });
