50f933a4e28cafe0682d124ee198474fc548ff78
root <root@li479-42.members.linode.com>
2014-04-30 03:02:19 -0400

    scripts

--- minefiles.sh
+++ minefiles.sh
@@ -1,4 +1,4 @@
-for X in exports/*.data
+for X in exports_02/*.data
 do
     perl minefile.pl $X || exit
 done
--- query.sh
+++ query.sh
@@ -1,3 +1,4 @@
 phantomjs --cookies-file=./cookies.txt query_isbn_list.js
+echo "Robot stopped." | mail -s "ROBOT STOPPED" alertdavid@portcompass.com
 
 
