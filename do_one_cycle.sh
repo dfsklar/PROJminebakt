5388f0536057ff9fde43e5687eb07f9cf2766cef
root <root@li479-42.members.linode.com>
2014-04-30 23:35:22 -0400

    Working version

--- do_one_cycle.sh
+++ do_one_cycle.sh
@@ -1,4 +1,17 @@
 echo "SELECT Id FROM inventory WHERE qtyallwh>0 and Type='Book' and BOOLneedsDetailsDownload='Y' LIMIT 2000" \
-   | sh mysql.sh --skip-column-names  > isbnlist_one_cycle.txt
-sh minefiles.sh > $$.sql
-sh mysql.sh < $$.sql   &&    mv $$.sql DONEALREADY
+     | sh mysql.sh --skip-column-names  > isbnlist_one_cycle.txt
+phantomjs --cookies-file=./cookies.txt query_isbn_list.js
+echo "" > $$.sql
+for X in exports/*.data
+do
+    perl minefile.pl $X >> $$.sql || exit
+done
+for X in exports/*.zero
+do
+    perl minezero.pl $X >> $$.sql || exit
+done
+echo DOC1
+sh mysql.sh < $$.sql   &&   mv $$.sql DONEALREADY   &&  mv exports DONEALREADY/exports_$$
+echo DOC2
+mkdir exports
+
